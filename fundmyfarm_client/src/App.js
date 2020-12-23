81d3c5e8e9d22dc80fbc3b445b8afddfca39625a
Umar Muhammad Zakari <umarfarouqft@gmail.com>
2020-12-22 17:14:12 -0800

    Client side up and runing

--- fundmyfarm_client/src/App.js
+++ fundmyfarm_client/src/App.js
@@ -1,23 +1,30 @@
-import React,{useEffect,createContext,useReducer,useContext} from 'react';
-import {BrowserRouter as Router, Route, Redirect, useHistory } from "react-router-dom";
-import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
+import React, { useEffect, createContext, useReducer, useContext } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Home from "./components/Home"
-import Navbar from "./components/Navbar"
-import IotCenter from "./components/iotcenter/IotCenter"
-import LendingPage from "./components/Lending"
+import Home from "./shared/home/Home";
+import Overview from "./lender/screens/Overview";
+import LendingMarket from "./lender/screens/LendingMarket";
+import PaymentPage from "./lender/screens/PaymentPage";
+import Error404 from "./shared/error404/Error404";
 
 function App() {
   return (
-    <div className="App">
-    <Router>
-      <Navbar/>
-      <Route path="/" exact strict component={LendingPage}/>
-      <Route path="/iot-center" exact strict component={IotCenter}/>
-      <Route path="/lending-page" exact strict component={LendingPage} />
-    </Router>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="lender/lending-market" component={LendingMarket} />
+        <Route path="lender/overview" component={Overview} />
+        <Route path="lender/view-campaign/2" component={PaymentPage} />
+
+        {/* Private Route available to only authenticated User */}
+        {/* <PrivateRoute path="/dashboard" component={Dashboard} />
+        <PrivateRoute path="/new-resume" component={New_resume} />
+        <PrivateRoute path="/template-a" component={TemplateA} /> */}
+
+        {/* Error 404 - Page Not Found */}
+        <Route path="*" component={Error404} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
