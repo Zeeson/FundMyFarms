d10d47f32f047167d4f371ca008109ac84f26cc7
Umar Muhammad Zakari <umarfarouqft@gmail.com>
2020-12-24 14:08:46 -0800

    Lender dashboard

--- fundmyfarm_client/src/lender/screens/Overview.js
+++ fundmyfarm_client/src/lender/screens/Overview.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Navbar from "../components/Navbar";
-import Footer from "../../shared/components/Footer";
 
+import Portfolio from "../../assets/Lender/Portfolio.png";
+import Income from "../../assets/Lender/Income.png";
+import Expense from "../../assets/Lender/Expense.png";
+import Perc from "../../assets/Lender/Percent.png";
+
+import SmallRectangle from "../../assets/Lender/small-rectangle.svg";
+import SmallRectangleGray from "../../assets/Lender/small-rectangle-gray.svg";
 import "./Overview.css";
 
 const campaignData = [
@@ -79,140 +84,157 @@ function Overview({
   numberofInvestment = 4,
   averageInvestment = 39000,
   averageInterest = 3,
+  portfolio = { remaining: 41.7, repaid: 58.3 },
 }) {
   return (
-    <>
-      <Navbar />
-      <div className="lender-dashboard">
-        <main className="mt-5">
-          <h1>
-            Welcome Back, <span> {name}</span>
-          </h1>
-          <div className="campaign-list">
-            <p className="section-name">
-              Campaingn available for investing.
-              <Link to="/lender/lending-market">View More </Link>
-            </p>
-            <div className="campaigns-container d-flex justify-content-between mb-5">
-              {campaignData.map((campaign, index) => {
-                return (
-                  <div key={index} className="campaign-item px-3 py-4">
-                    <div className="basic-info">
-                      <div className="profile-photo">
-                        <img src="" alt="" />
-                      </div>
-                      <div className="name-n-age d-flex flex-column">
-                        <p className="name mb-0">{campaign.name}</p>
-                        <span className="age">{campaign.age}</span>
-                      </div>
+    <main className="lender-dashboard d-flex justify-content-between flex-column">
+      <h1>
+        Welcome Back, <span> {name}</span>
+      </h1>
+      <div className="campaign-list">
+        <p className="section-name">
+          Campaingn available for investing.
+          <Link to="/lender/lending-market">View More </Link>
+        </p>
+        <div className="d-flex justify-content-between campaigns-container">
+          <div className="campaign-items d-flex justify-content-between">
+            {campaignData.map((campaign, index) => {
+              return (
+                <div key={index} className="campaign-item py-4">
+                  <div className="basic-info">
+                    <div className="name-n-age d-flex justify-content-between align-items-center">
+                      <span className="name mb-0">{campaign.name}</span>
+                      <span className="age">{campaign.age}</span>
                     </div>
-                    <p className="loan-amount">
-                      <b>Loan amount: ₦ {campaign.loanAmount}</b>
-                    </p>
+                  </div>
+                  <p className="loan-amount d-flex justify-content-between">
+                    <span>Loan amount:</span>{" "}
+                    <span>₦ {campaign.loanAmount}</span>
+                  </p>
 
-                    <div className="loan-progress mb-4">
-                      <div className="progress mb-2">
-                        <div
-                          className="progress-bar"
-                          role="progressbar"
-                          style={{ width: `${campaign.currentAmountPer}%` }}
-                          aria-valuenow={campaign.currentAmountPer}
-                          aria-valuemin="0"
-                          aria-valuemax="100"
-                        ></div>
-                      </div>
-                      <div className="d-flex justify-content-between">
-                        <span className="current-amount">
-                          N {campaign.currentAmount}
-                        </span>
-                        <span className="amount-left">
-                          N {campaign.amountLeft}
-                        </span>
-                      </div>
+                  <div className="loan-progress">
+                    <div className="progress">
+                      <div
+                        className="progress-bar"
+                        role="progressbar"
+                        style={{ width: `${campaign.currentAmountPer}%` }}
+                        aria-valuenow={campaign.currentAmountPer}
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      ></div>
                     </div>
+                    <div className="d-flex justify-content-between">
+                      <span className="current-amount">
+                        N {campaign.currentAmount}
+                      </span>
+                      <span className="amount-left">
+                        N {campaign.amountLeft}
+                      </span>
+                    </div>
+                  </div>
 
-                    <Link to="view-campaign/2" className="invest-button">
-                      <button className="btn btn-primary">Invest Now</button>
-                    </Link>
-                  </div>
-                );
-              })}
-              <div className="campaign-item d-flex align-items-center justify-content-center">
-                View more
-              </div>
-            </div>
-          </div>
-          <div className="investment-portfolio mb-5">
-            <p className="section-name">Here is your portfolio overview.</p>
-            <div className="d-flex justify-content-between">
-              <div className="portfolio-unit">
-                <div className="portfolio-column d-flex justify-content-between">
-                  <div className="portfolio-item total-investment px-3 py-3">
-                    <p className="portfolio-title">Invested Amount</p>
-                    <p className="amount-title">{totalInvestment}</p>
-                  </div>
-                  <div className="portfolio-item repaidAmount px-3 py-3">
-                    <p className="portfolio-title">Repaid Amount</p>
-                    <p className="amount-title">{repaidAmount}</p>
-                  </div>
-                  <div className="portfolio-item remainingAmount px-3 py-3">
-                    <p className="portfolio-title">Remaining Amount</p>
-                    <p className="amount-title">{remainingAmount}</p>
-                  </div>
+                  <Link to="view-campaign/2" className="invest-button">
+                    <button className="btn btn-primary">Invest Now</button>
+                  </Link>
                 </div>
-                <div className="portfolio-column d-flex justify-content-between mt-2">
-                  <div className="portfolio-item number-of-investment px-3 py-3">
-                    <p className="portfolio-title">Investments</p>
-                    <p className="amount-title">{numberofInvestment}</p>
-                  </div>
-                  <div className="portfolio-item average-investment px-3 py-3">
-                    <p className="portfolio-title">Average Investment</p>
-                    <p className="amount-title">{averageInvestment}</p>
-                  </div>
-                  <div className="portfolio-item average-interest px-3 py-3">
-                    <p className="portfolio-title">Average Interest</p>
-                    <p className="amount-title">{averageInterest}</p>
-                  </div>
-                </div>
-              </div>
-              <div className="portfolio-chart">pie chart</div>
-            </div>
+              );
+            })}
           </div>
-          <div className="account-overview">
-            <p className="section-name">Here is your account overview</p>
-            <table className="table">
-              <thead class="thead-green">
-                <tr>
-                  <th scope="col">LOAN</th>
-                  <th scope="col">AMOUNT</th>
-                  <th scope="col">INTEREST RATE</th>
-                  <th scope="col">TERM</th>
-                  <th scope="col">TOTAL RETURNS</th>
-                  <th scope="col">PAYMENT DUE</th>
-                  <th scope="col">STATUS</th>
-                </tr>
-              </thead>
-              <tbody>
-                {theData.map((loandata, index) => {
-                  return (
-                    <tr key={index}>
-                      <th scope="row">{loandata.loanId}</th>
-                      <td>{loandata.amount}</td>
-                      <td>{loandata.rate}</td>
-                      <td>{loandata.term}</td>
-                      <td>{loandata.return}</td>
-                      <td>{loandata.due}</td>
-                      <td>{loandata.status}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+
+          <div className="view-more-campaigns d-flex align-items-center justify-content-center">
+            View more
           </div>
-        </main>
+        </div>
       </div>
-      <Footer />
-    </>
+      <div className="investment-portfolio mb-5">
+        <p className="section-name">Here is your portfolio overview.</p>
+
+        <div className="portfolio-unit">
+          <div className="portfolio-column d-flex justify-content-between mb-5">
+            <div className="portfolio-item d-flex total-investment align-items-center">
+              <div className="portfolio-img">
+                <img src={Portfolio} alt="portfolio img" />
+              </div>
+              <div className="portfolio-info">
+                <p className="portfolio-title">Invested Amount</p>
+                <p className="amount-title mb-0">{totalInvestment}</p>
+              </div>
+            </div>
+            <div className="portfolio-item d-flex align-items-center repaidAmount">
+              <div className="portfolio-img">
+                <img src={Income} alt="income img" />
+              </div>
+              <div className="portfolio-info">
+                <p className="portfolio-title">Repaid Amount</p>
+                <p className="amount-title mb-0">{repaidAmount}</p>
+              </div>
+            </div>
+            <div className="portfolio-item d-flex align-items-center remainingAmount">
+              <div className="portfolio-img">
+                <img src={Expense} alt="expence img" />
+              </div>
+              <div className="portfolio-info">
+                <p className="portfolio-title">Remaining Amount</p>
+                <p className="amount-title mb-0">{remainingAmount}</p>
+              </div>
+            </div>
+          </div>
+          <div className="portfolio-column d-flex justify-content-between">
+            <div className="portfolio-item d-flex total-investment align-items-center">
+              <div className="portfolio-img">
+                <img src={Portfolio} alt="investment img" />
+              </div>
+              <div className="portfolio-info">
+                <p className="portfolio-title">Investments</p>
+                <p className="amount-title mb-0">{numberofInvestment}</p>
+              </div>
+            </div>
+            <div className="portfolio-item d-flex align-items-center repaidAmount">
+              <div className="portfolio-img">
+                <img src={Perc} alt="invest img" />
+              </div>
+              <div className="portfolio-info">
+                <p className="portfolio-title">Average Investmentt</p>
+                <p className="amount-title mb-0">{averageInvestment}</p>
+              </div>
+            </div>
+            <div className="portfolio-item d-flex align-items-center remainingAmount">
+              <div className="portfolio-img">
+                <img src={Perc} alt="interest img" />
+              </div>
+              <div className="portfolio-info">
+                <p className="portfolio-title">Average Interest</p>
+                <p className="amount-title mb-0">{averageInterest}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="repayment-chart">
+          <div className="progress">
+            <div
+              className="progress-bar"
+              role="progressbar"
+              style={{ width: `${portfolio.remaining}%` }}
+              aria-valuenow={portfolio.remaining}
+              aria-valuemin="0"
+              aria-valuemax="100"
+            ></div>
+          </div>
+          <div className="chart-units d-flex justify-content-center">
+            <div className="remaining d-flex pr-5">
+              <img src={SmallRectangle} alt="remaining img" className="pr-3" />
+              <span>% {portfolio.remaining} Remaining</span>
+            </div>
+            <div className="repaid">
+              <img src={SmallRectangleGray} alt="repaid img" className="pr-3" />
+              <span>% {portfolio.repaid} Repaid</span>
+            </div>
+          </div>
+          <p className="text-center mt-3">Investment Payment Chart</p>
+        </div>
+      </div>
+    </main>
   );
 }
 
